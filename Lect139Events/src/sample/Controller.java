5ec672439670eee7a88d7f2cfd6cf44a787d3a6d
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-26 01:12:12 +0200

    More notes in README.

--- Lect139Events/src/sample/Controller.java
+++ Lect139Events/src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
@@ -9,8 +10,9 @@ public class Controller {
 	private TextField nameField;
 	
 	@FXML
-	public void onButtonClicked(){
+	public void onButtonClicked(ActionEvent e){
 		System.out.println("Keep on fighting " + nameField.getText());
+		System.out.println("The following button was pressed " + e.getSource());
 	}
 
 }
